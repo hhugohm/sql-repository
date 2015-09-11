b0e0f6adfca6d60b2e3ad41ab9ebffe7a117dfbe
hhugohm <hhugohm@gmail.com>
2015-09-11 17:15:03 -0500

    Update joing.sql
    cambio join

--- oracle12c-certification/unit3-restricting-sorting/joing.sql
+++ oracle12c-certification/unit3-restricting-sorting/joing.sql
@@ -12,7 +12,7 @@ Los tres pilares de la teoria relacional son:
 Las tecnicas ocupadas para realizar los joins son:
 1.- equijoin
 2.- nonequijoin
-3.- selftjoin: Asociar rows con otras rows de la misma tabla.
+3.-     selftjoin: Asociar rows con otras rows de la misma tabla.
 
 inner join: Cuando se realiza la asociacion por medio de un match entre tabla source y target. 
 			Donde el valor de la columna que hace match en la tabla source aparece de igualmente en la tabla target.
@@ -34,6 +34,8 @@ natural join table2;
 -la relacion en el natural join sera en automatico
 -No se podra usar qualifiers (alias o nombre completo tabla.nombreCampo) en los campos join.
 
+select * from regions natural joing countries where country_id='US';
+select employee_id, job_id, department_id, emp.last_name, emp.hire_date, jh.end_date from join job_history jh   natural join employees emp; 
 select region_name from regions natural join countries where country_name='Canada';
 select country_name from countries natural join regions where 	region_name='America';
  
@@ -52,7 +54,7 @@ select table1.column,table2.column
 from table1
 join table2 using(join_column1,join_colum2...);
 
---la relacion en el natural join se debe hacer de forma explicita
+--la relacion en el  join using  se debe hacer de forma explicita
 --No se podra usar qualifiers(alias o nombre completo tabla.nombreCampo) en los campos join.
 --No se puede usar en conjunto con el natural
 
@@ -66,11 +68,48 @@ using (id_bitacora,id_subj_sys)
 where id_subj_sys=1073014 and trunc(date_trans)=to_date('10-09-15')
 order by id;
 
+@@@@@@@
+join on: Es una forma explicita de realizar el joing de las columnas asi como una forma mas flexible.
+@@@@@@@
+select table1.column, table2.column
+from table1
+join table2 on (table1.column_name=table2.column_name);
 
+--Si se podra usar qualifiers(alias o nombre completo tabla.nombreCampo) en los campos join.
+--No se puede usar en conjunto con el natural
 
+select * from departments d join employees e on(e.employee_id=d.department_id);
+select * from emplloyees e , departments d where e.employee_id=d.department_id;
 
+--union de dos tablas
+select bitacora.id_bitacora id , dtl.id_subj_sys id_person ,bitacora.id_sistema,dtl.level_coincidence
+from tbl_watchlist_bitacora bitacora
+join tbl_watchlist_bitacora_dtl  dtl 
+on (bitacora.id_bitacora=dtl.id_bitacora and bitacora.id_subj_sys=dtl.id_subj_sys) --se unen dos columnas
+where  bitacora.id_subj_sys=1073014 and trunc(bitacora.date_trans)=to_date('10-09-15')
+order by id;
 
+--union de tres tablas
+select bitacora.id_bitacora id , dtl.id_subj_sys id_person ,wlista.nomcomp,bitacora.id_sistema,dtl.level_coincidence
+from tbl_watchlist_bitacora bitacora
+join tbl_watchlist_bitacora_dtl  dtl 
+on (bitacora.id_bitacora=dtl.id_bitacora and bitacora.id_subj_sys=dtl.id_subj_sys) --se unen dos columnas
+join tbl_watchlist wlista
+on (dtl.id_subj_wl= wlista.id_subj_wl)
+where  bitacora.id_subj_sys=1073014 and trunc(bitacora.date_trans)=to_date('10-09-15')
+order by id;
 
+select bitacora.id_bitacora id , dtl.id_subj_sys person ,bitacora.id_sistema,dtl.level_coincidence
+from tbl_watchlist_bitacora bitacora,tbl_watchlist_bitacora_dtl  dtl
+where bitacora.id_bitacora=dtl.id_bitacora and  bitacora.id_subj_sys=dtl.id_subj_sys and
+bitacora.id_subj_sys=1073014 and trunc(bitacora.date_trans)=to_date('10-09-15')
+order by id;
 
+--union de tres tablas / oracle standar
+select bitacora.id_bitacora id , dtl.id_subj_sys person ,wlista.nomcomp,bitacora.id_sistema,dtl.level_coincidence
+from tbl_watchlist_bitacora bitacora,tbl_watchlist_bitacora_dtl  dtl, tbl_watchlist wlista
+where bitacora.id_bitacora=dtl.id_bitacora and  bitacora.id_subj_sys=dtl.id_subj_sys and dtl.id_subj_wl= wlista.id_subj_wl and
+bitacora.id_subj_sys=1073014 and trunc(bitacora.date_trans)=to_date('10-09-15')
+order by id;
 
-
+(55) 11427711 --> Sandra
